09147709e8588d18469c31c3fbbb341c6c4b61a7
sarcasternub <sarcasteryt@gmail.com>
2023-09-12 23:59:52 +0530

    fix metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -39,6 +39,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta property="og:title" content={embedTitle} />
 				<meta property="og:url" content={url} />
 				<meta property="og:description" content={description} />
+				<meta property="og:image" content={image} />
 				<meta property="og:color" content={color} />
 				<meta property="twitter:card" content="summary_large_image" />
 				<meta name="twitter:image" content={image} />
