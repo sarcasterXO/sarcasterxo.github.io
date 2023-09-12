6482b13f61e236e871f9122dd960dfb89b98bd77
sarcasternub <sarcasteryt@gmail.com>
2023-09-12 23:27:20 +0530

    update metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,10 +37,11 @@ export default function App({ Component, pageProps }: AppProps) {
 				<link rel="icon" href="/favicon.ico" />
 				<meta content="width=device-width, initial-scale=1" name="viewport" />
 				<meta property="og:title" content={embedTitle} />
-				<meta property="og:description" content={description} />
 				<meta property="og:url" content={url} />
-				<meta property="og:image" content={image} />
+				<meta property="og:description" content={description} />
 				<meta property="og:color" content={color} />
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta name="twitter:image" content={image} />
 			</Head>
 			<Component {...pageProps} />
 			<Footer />
